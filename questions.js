22d7f780e81b642ba54c6669170cf88c38e857ec
dfordivyansh <dfordivyansh3@gamil.com>
2023-08-22 11:52:44 +0530

    first commit

---
+++ questions.js
@@ -0,0 +1,119 @@
+
+let questions = [
+   {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Markup Language",
+      "Hyper Text Preprocessor",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+     ]
+    },
+
+    {
+      numb: 2,
+      question: "Which CSS property is used to change the text color of an element?",
+      answer: "color",
+      options: [
+        "font-color",
+        "color",
+        "text-color",
+        "text-style"
+      ]
+    },
+    {
+      numb: 3,
+      question: "What is the correct way to comment out multiple lines of code in HTML and CSS",
+      answer: "/* This is a comment */",
+      options: [
+        "// This is a comment",
+        "/* This is a comment */",
+        "// This is a comment //",
+        "{ This is a comment }"
+      ]
+    },
+      {
+      numb: 4,
+      question: "What does CSS stand for?",
+      answer: "Cascading Style Sheet",
+      options: [
+        "Common Style Sheet",
+        "Colorful Style Sheet",
+        "Computer Style Sheet",
+        "Cascading Style Sheet"
+      ]
+    },
+    {
+      numb: 5,
+      question: "In CSS, which property is used to control the space between lines of text within an element?",
+      answer: "line-height",
+
+      options: [
+       "word-spacing",
+       "letter-spacing",
+       "line-height",
+       "text-spacing"
+      ]
+    },
+    {
+      numb: 6,
+      question: "Which CSS property is used to make text bold?",
+      answer: "font-weight",
+      options: [
+       "font-style",
+       "font-weight",
+       "text-weight",
+       "bold-text"
+      ]
+    },
+    {
+      numb: 7,
+      question: "Which CSS property is used to control the spacing between elements on the page, like margins and padding?",
+      answer: "box-model",
+
+      options: [
+       "spacing",
+       "distance",
+       "margin-padding",
+       "box-model"
+      ]
+    },
+    {
+      numb: 8,
+      question: "In CSS, what property is used to control the transparency of an element?",
+      answer: "opacity",
+
+      options: [
+       "opacity",
+       "transparency",
+       "visibility",
+       "blur"
+      ]
+    },
+    {
+      numb: 9,
+      question: "In HTML, what does the term semantics refer to?",
+      answer: "The meaning and structure of the content",
+
+      options: [
+       "The visual appearance of a web page",
+       "The meaning and structure of the content",
+       "The speed at which a web page loads",
+       "The interaction between HTML and CSS"
+      ]
+    },
+    {
+      numb: 10,
+      question: "Which of the following is not a valid character entity reference in HTML?",
+      answer: "&strike",
+
+      options: [
+       "©",
+       "<",
+       " &",
+       "&strike"
+      ]
+    }
+  ];
